28b11f28cf46513f15fb05f1fdad832e2426f166
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-03-29 15:48:47 -0300

    at implemented

--- linked-list.rb
+++ linked-list.rb
@@ -10,25 +10,51 @@ end
 ##############################
 
 class LinkedList
-  attr_accessor :head, :size
+  attr_accessor :head, :size, :tail
 
   def initialize
     @head = nil
+    @tail = nil
     @size = 0
   end
 
   def add(data)
     if @head.nil?
       @head = Node.new(data)
+      @tail = @head
       @size += 1
     else
       pointer = @head
-      while !pointer.next.nil?
+      until pointer.next.nil?
         pointer = pointer.next
       end
-      pointer.next = Node.new(data)
+      @tail = Node.new(data)
+      pointer.next = @tail
+      @size += 1
     end
   end
+
+  def at(index)
+    pointer = @head
+
+    if index == 0
+      @head.value
+    else
+      for i in 0..index
+        if !pointer.nil?
+          pointer = pointer.next
+        else
+          return "List index out of range"
+        end
+      end
+      if !pointer.nil?
+        pointer.value
+      else
+        return "List index out of range"
+      end
+    end
+  end
+
 
 end
 
