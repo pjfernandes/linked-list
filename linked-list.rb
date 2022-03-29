190118ccb3bec4bb147621f2c7a4ef6640deb3c7
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-03-29 17:23:37 -0300

    find implemented

--- linked-list.rb
+++ linked-list.rb
@@ -36,18 +36,17 @@ class LinkedList
 
   def at(index)
     pointer = @head
-
     if index == 0
       @head.value
     else
-      for i in 0..index
-        if !pointer.nil?
+      for i in 0...index
+        unless pointer.nil?
           pointer = pointer.next
         else
           return "List index out of range"
         end
       end
-      if !pointer.nil?
+      unless pointer.nil?
         pointer.value
       else
         return "List index out of range"
@@ -55,6 +54,56 @@ class LinkedList
     end
   end
 
+  def pop
+    pointer = @head
+    if pointer.nil?
+      "Empty list"
+    else
+      until pointer.next.nil?
+        pointer = pointer.next
+        value = pointer.value
+      end
+      pointer.value = nil
+      @size -= 1
+      @tail = self.node_at(@size - 1)
+    end
+    value
+  end
+
+  def find(value)
+    pointer = @head
+    if pointer.nil?
+      "Empty list"
+    else
+      i = 0
+      until pointer.value == value
+        pointer = pointer.next
+        i += 1
+      end
+    end
+    "#{pointer.value} found at #{i} index"
+  end
+
+  private
+  def node_at(index)
+    pointer = @head
+    if index == 0
+      @head
+    else
+      for i in 0...index
+        unless pointer.nil?
+          pointer = pointer.next
+        else
+          return "List index out of range"
+        end
+      end
+      unless pointer.nil?
+        pointer
+      else
+        return "List index out of range"
+      end
+    end
+  end
 
 end
 
